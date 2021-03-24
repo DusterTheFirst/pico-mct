5ab097baf936a9a7233b4ea4a32a883ab03d4534
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2021-03-24 11:50:10 -0400

    Follow Tutorial

--- web/src/app.js
+++ web/src/app.js
@@ -3,9 +3,68 @@ window.onload = () => {
     openmct.install(openmct.plugins.LocalStorage());
     openmct.install(openmct.plugins.MyItems());
     openmct.install(openmct.plugins.UTCTimeSystem());
-    openmct.time.clock('local', { start: -15 * 60 * 1000, end: 0 });
-    openmct.time.timeSystem('utc');
+    openmct.time.clock("local", { start: -15 * 60 * 1000, end: 0 });
+    openmct.time.timeSystem("utc");
     openmct.install(openmct.plugins.Espresso());
 
+    openmct.install(PicoPilotPlugin());
+
     openmct.start();
+};
+
+const namespace = "dusterthefirst.pico-pilot";
+
+
+function PicoPilotPlugin() {
+    return (openmct) => {
+        // console.log("I've ben installed!");
+
+        openmct.objects.addRoot({
+            namespace,
+            key: "avionics"
+        });
+
+        openmct.objects.addProvider(namespace, objectProvider);
+
+        openmct.types.addType(`${namespace}.telemetry`, {
+            name: 'Example Telemetry Point',
+            description: 'Example telemetry point from our happy tutorial.',
+            cssClass: 'icon-telemetry'
+        });
+
+        openmct.composition.addProvider(compositionProvider);
+    }
+}
+
+const objectProvider = {
+    get: async (identifier) => {
+        if (identifier.key === "avionics") {
+            return {
+                identifier,
+                name: "Pico Pilot",
+                type: "folder",
+                location: "ROOT"
+            }
+        } else {
+            return {
+                identifier,
+                name: `Pico Pilot (${identifier.key})`,
+                type: `${namespace}.telemetry`,
+                location: `${namespace}:avionics`
+            }
+        }
+
+    }
+};
+
+const compositionProvider = {
+    appliesTo: (object) => {
+        return object.identifier.namespace === namespace && object.type === "folder";
+    },
+    load: async () => {
+        return [{
+            namespace,
+            key: 1
+        }]
+    }
 };
